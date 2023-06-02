a706f5b0c5a0166a3fe023261a405f2c44c388e5
Milena Maganin <mmaganin@mcfadyen.com>
2023-06-02 04:10:22 +0100

    apply logics and contents to all dice

--- js/api.js
+++ js/api.js
@@ -1,69 +1,82 @@
-const randomScenarios = Array.from({ length: 6 }, function() {
-  return (Math.floor(Math.random() * 100) + 1);
-});
+// const randomScenarios = Array.from({ length: 6 }, function() {
+//   return (Math.floor(Math.random() * 100) + 1);
+// });
 
-const scenarios = document.querySelectorAll(".scenario");
+// const scenarios = document.querySelectorAll(".scenario");
 
-scenarios.forEach(function(element, index) {
-  const randomScenario = randomScenarios[index];
-  element.style.backgroundImage = `url("https://picsum.photos/id/${randomScenario}/200")`;
-});
+// scenarios.forEach(function(element, index) {
+//   const randomScenario = randomScenarios[index];
+//   element.style.backgroundImage = `url("https://picsum.photos/id/${randomScenario}/200")`;
+// });
 
-// fetch('https://api.unsplash.com/photos/random?query=object', {
+// GET CHARACTERS FROM SUPERHERO API //
+// function getRandomHero(element) {
+//   const randomHeroId = Math.floor(Math.random() * 731) + 1;
+//   const apiUrl = `https://superheroapi.com/api.php/6810662638961832/${randomHeroId}`;
+
+//   fetch(apiUrl)
+//     .then(response => response.json())
+//     .then(data => {
+//       const imageUrl = data.image.url;
+//       element.style.backgroundImage = `url(${imageUrl})`;
+//     })
+//     .catch(error => {
+//       console.log('Ops, we failed you', error);
+//     });
+// }
+
+// // APPLY CHARACTERS #1 AND #2 //
+// const characterOne = document.querySelectorAll(".character.one");
+// const characterTwo = document.querySelectorAll(".character.two");
+
+// characterOne.forEach(character => {
+//   getRandomHero(character);
+// });
+
+// characterTwo.forEach(character => {
+//   getRandomHero(character);
+// });
+
+// // GET MOVIES FROM IMDB API //
+
+// // var requestOptions = {
+// //   method: 'GET',
+// //   redirect: 'follow'
+// // };
+
+// // fetch('https://imdb-api.com/en/API/Top250Movies/k_2c0pz6sl')
+// //   .then(response => response.json())
+// //   .then(data => {
+// //     const filmTitle = data;
+// //     console.log(filmTitle); 
+
+// //   })
+// //   .catch(error => {
+// //     console.log('Ocorreu um erro:', error);
+// //   });
+
+// const options = {
+//   method: 'GET',
 //   headers: {
-//     Authorization: 'Client-ID 1zfcnCeelLmlT704xeVPmWsqtvYeZq6fdhvcfQVouAg'
+//     accept: 'application/json',
+//     Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkMGFmZTg0NTliMTBiZDRlNGU2NjQwMTdmNGM5Njc3NyIsInN1YiI6IjY0NzUzNWVmYzI4MjNhMDBhOGQ0Nzk1MiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.H6mqizukTf4Q-kZocxkcp-RpzflWp5cGyQEQJkf00Ps'
 //   }
-// })
-//   .then(response => response.json())
-//   .then(data => {
-//     const objects = document.querySelectorAll(".object");
-//     // objects.style.background = data
-//     console.log(data);
-//   })
-//   .catch(error => {
-//     console.error(error);
-//   });
-
-// const crypto = require('crypto');
-// const publicKey = '6547a23f165c59b9f23d9e30048fa66f';
-// const privateKey = '6862883225799a0e5de143f796d5dcbec34531f3';
-// const ts = Date.now().toString();
-// const hash = crypto.createHash('md5').update(ts + privateKey + publicKey).digest('hex');
-// const url = require('url');
-// const baseUrl = 'http://gateway.marvel.com/v1/public/characters';
-// const queryParams = {
-//   ts: ts,
-//   apikey: publicKey,
-//   hash: hash
 // };
-// const apiUrl = url.format({ pathname: baseUrl, query: queryParams });
 
-function getRandomHeroImage(element) {
-  const randomHeroId = Math.floor(Math.random() * 731) + 1;
-  const apiUrl = `https://superheroapi.com/api.php/6810662638961832/${randomHeroId}`;
+// REFRESH BUTTONS //
+// function addRefreshButtonEventListener(refreshButton, characters) {
+//   refreshButton.addEventListener("click", () => {
+//     characters.forEach(character => {
+//       getRandomHero(character);
+//     });
+//   });
+// }
 
-  fetch(apiUrl)
-    .then(response => response.json())
-    .then(data => {
-      const imageUrl = data.image.url;
-      element.style.backgroundImage = `url(${imageUrl})`;
-    })
-    .catch(error => {
-      console.log('Ops, we failed you', error);
-    });
-}
+// const refreshButtonOne = document.querySelector(".refresh-button.one");
+// const refreshButtonTwo = document.querySelector(".refresh-button.two");
 
-const characterOne = document.querySelectorAll(".character.one");
-const characterTwo = document.querySelectorAll(".character.two");
-
-characterOne.forEach(character => {
-  getRandomHeroImage(character);
-});
-
-characterTwo.forEach(character => {
-  getRandomHeroImage(character);
-});
-
+// addRefreshButtonEventListener(refreshButtonOne, characterOne);
+// addRefreshButtonEventListener(refreshButtonTwo, characterTwo);  
 
 
 
